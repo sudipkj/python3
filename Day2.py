dc6ba4767ba00e2f561af21d40a1e62be3f18cec
Sudipkj <sudipkj@gmail.com>
2025-07-26 05:21:56 +0530

    Initial commit - ML day 2

---
+++ Day2.py
@@ -0,0 +1,15 @@
+list = [40,30,'Aa']
+print(list)
+
+list.append(3)
+print(list)
+
+# List.append doesn't return the list itself, it returns None
+# list = list.append(4)
+# print(list)
+
+list.pop(-2)
+print(list)
+
+list.pop()
+print(list)
