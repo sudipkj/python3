e3753fbba50245a35badcb363509090923df7163
Sudipkj <sudipkj@gmail.com>
2025-07-27 23:45:48 +0530

    Initial commit - ML day 2 list and dict

--- Day2.py
+++ Day2.py
@@ -13,3 +13,67 @@ print(list)
 
 list.pop()
 print(list)
+
+list.insert(1, 100)
+print(list)
+
+list2 = [1, 2, 3]
+list += list2
+print(list)
+list.append(list2)
+print(list)
+
+list.extend('Python')
+print(list)
+
+
+for  idx , element in enumerate(list):
+    if(element == 't'):
+        list.pop(idx)
+print(list)
+
+list.remove('P')
+print(list)
+
+list.insert(2,'Hello')
+print(list)
+
+list.insert(2,['Hello'])
+print(list)
+
+# Dictionaries
+
+person = {
+    'name': 'John',
+    'age': 30,
+    'city': 'New York'
+}
+
+person['hobbies'] = ['reading', 'traveling']
+print(person)
+print(person.items())
+print(person.keys())
+print(person.values())
+
+cities = ["New York", "Los Angeles", "Chicago"]
+areas = [8.4, 3.9, 2.7]  # in millions
+
+city_areas = dict(zip(cities, areas))
+print(city_areas)
+
+print(tuple(zip(cities, areas)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
