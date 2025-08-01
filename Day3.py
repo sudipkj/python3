d89b955f1a4d9fea663c01429164ee7bb9895943
Sudipkj <sudipkj@gmail.com>
2025-08-01 08:42:14 +0530

    Initial commit - ML day 3 plot program

--- Day3.py
+++ Day3.py
@@ -12,10 +12,30 @@ n_dataset = 30
 n_sample = 20
 noise_std =0.3
 x_plot = np.linspace(0, 1, 100)
-print(x_plot)
-
 def true_function(x):
-    return np.sin(2 * np.pi * x)
+    return np.sin(2 * np.pi * x) #2*pi is used to create a full sine wave cycl
 
-degree = [1]
+degree = [1, 4]
+
+for index, degree in enumerate(degree):
+    predictions= []
+    for j in range(n_dataset):
+        # print(j, index, degree)
+        x = np.sort(np.random.rand(n_sample))
+        y = true_function(x) + np.random.normal(0, noise_std, n_sample)
+
+        #define the model
+        model = mp(PolynomialFeatures(degree), LinearRegression())
+        model.fit(x[:, np.newaxis], y)
+        y_predict = model.predict(x_plot[:, np.newaxis])
+
+        predictions.append(y_predict)
+
+    predictions = np.array(predictions)
+    mean_prediction = predictions.mean(axis=0)
+    variance = predictions.var(axis=0)
+
+    for pred in predictions:
+        plt.plot(x_plot, pred, color='gray')
+    plt.plot(x_plot, true_function(x_plot), 'red', label='True Function')
 
